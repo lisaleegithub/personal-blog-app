756ded95d76875ad9761a0b3126dbd91e9804bfc
Lisa Lee <lisawlee04@gmail.com>
2022-04-05 23:05:35 -0700

    GET request works to show the list of blog posts.

--- server/server.js
+++ server/server.js
@@ -9,41 +9,32 @@ const PORT = 5005;
 app.use(cors());
 app.use(express.json());
 
-//creates an endpoint for the route /api
+// creates an endpoint for the route /api
 app.get('/', (req, res) => {
     res.json({ message: 'Hello from My ExpressJS' });
 });
 
-//create the get request
-app.get('/api/students', cors(), async (req, res) => {
-    // const STUDENTS = [
-
-    //     { id: 1, firstName: 'Lisa', lastName: 'Lee' },
-    //     { id: 2, firstName: 'Eileen', lastName: 'Long' },
-    //     { id: 3, firstName: 'Fariba', lastName: 'Dako' },
-    //     { id: 4, firstName: 'Cristina', lastName: 'Rodriguez' },
-    //     { id: 5, firstName: 'Andrea', lastName: 'Trejo' },
-    // ];
-    // res.json(STUDENTS);
+// GET request for the posts table
+app.get('/api/posts', cors(), async (req, res) => {
     try{
-        const { rows: students } = await db.query('SELECT * FROM students');
-        res.send(students);
+        const { rows: posts } = await db.query('SELECT * FROM posts');
+        res.send(posts);
     } catch (e){
         return res.status(400).json({e});
     }
 });
 
-//create the POST request
-app.post('/api/students', cors(), async (req, res) => {
-    const newUser = { firstname: req.body.firstname, lastname: req.body.lastname }
-    console.log([newUser.firstname, newUser.lastname]);
-    const result = await db.query(
-        'INSERT INTO students(firstname, lastname) VALUES($1, $2) RETURNING *',
-        [newUser.firstname, newUser.lastname]
-    );
-    console.log(result.rows[0]);
-    res.json(result.rows[0]);
-});
+// // POST request
+// app.post('/api/students', cors(), async (req, res) => {
+//     const newUser = { firstname: req.body.firstname, lastname: req.body.lastname }
+//     console.log([newUser.firstname, newUser.lastname]);
+//     const result = await db.query(
+//         'INSERT INTO students(firstname, lastname) VALUES($1, $2) RETURNING *',
+//         [newUser.firstname, newUser.lastname]
+//     );
+//     console.log(result.rows[0]);
+//     res.json(result.rows[0]);
+// });
 
 // console.log that your server is up and running
 app.listen(PORT, () => {
